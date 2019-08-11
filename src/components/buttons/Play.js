0e05cad4cc54102e131f145e33b2acaeb1bb17ab
Dian Niu <zn15@duke.edu>
2019-08-10 22:32:55 -0400

    deleted unneeded imports from Play.js

--- src/components/buttons/Play.js
+++ src/components/buttons/Play.js
@@ -1,15 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Fab from '@material-ui/core/Fab';
-import AddIcon from '@material-ui/icons/Add';
-import Dialog from '@material-ui/core/Dialog';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
-import CloseIcon from '@material-ui/icons/Close';
-import SaveIcon from '@material-ui/icons/Save';
-import Slide from '@material-ui/core/Slide';
-import ConfigTable from '../pages/ConfigTable';
 import PlayIcon from '@material-ui/icons/PlayArrow';
 
 const useStyles = makeStyles(theme => ({
