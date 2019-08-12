209cb4c355d7bada8128fac634095c96287928bd
Joe Johans <lgvichy@gmail.com>
2019-08-12 00:17:11 -0700

    Fix graph check box integration

--- src/components/objects/Checkbox.js
+++ src/components/objects/Checkbox.js
@@ -1,52 +1,31 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import FormLabel from '@material-ui/core/FormLabel';
-import FormControl from '@material-ui/core/FormControl';
-import FormGroup from '@material-ui/core/FormGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormHelperText from '@material-ui/core/FormHelperText';
-import Checkbox from '@material-ui/core/Checkbox';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import FormLabel from "@material-ui/core/FormLabel";
+import FormControl from "@material-ui/core/FormControl";
+import FormGroup from "@material-ui/core/FormGroup";
+import FormHelperText from "@material-ui/core/FormHelperText";
 
 const useStyles = makeStyles(theme => ({
-    root: {
-      display: 'flex',
-      borderLeft: '1px solid gray',
-      height: "100%"
-    },
-    formControl: {
-      margin: theme.spacing(3)
-    },
+  root: {
+    display: "flex",
+    borderLeft: "1px solid gray",
+    height: "100%"
+  },
+  formControl: {
+    margin: theme.spacing(3)
+  }
 }));
 
-export default function CheckboxesGroup({onCheck, graphNames}) {
+export function CheckBoxGroup({ children }) {
   const classes = useStyles();
 
-  const toggles = Object.entries(graphNames).map(([name, toggleOn]) => 
-                    <FormControlLabel
-                    control={<Checkbox checked={name} onChange={onCheck} value={toggleOn} />}
-                    label={name}
-                />
-                )
-        
-    return(
-        <div className={classes.root}>
-            <FormControl component="fieldset" className={classes.formControl}>
-                <FormLabel component="legend">Model Configurations</FormLabel>
-                <FormGroup>
-                  {toggles}
-                {/* <FormControlLabel
-                    control={<Checkbox checked={gilad} onChange={handleChange} value="gilad" />}
-                    label="Gilad Gray"
-                />
-                <FormControlLabel
-                    control={
-                    <Checkbox checked={antoine} onChange={onCheck} value="antoine" />
-                    }
-                    label="Antoine Llorca"
-                /> */}
-                </FormGroup>
-                <FormHelperText>Choose configurations to display</FormHelperText>
-            </FormControl>
-        </div>
-    );
+  return (
+    <div className={classes.root}>
+      <FormControl component="fieldset" className={classes.formControl}>
+        <FormLabel component="legend">Model Configurations</FormLabel>
+        <FormGroup>{children}</FormGroup>
+        <FormHelperText>Choose configurations to display</FormHelperText>
+      </FormControl>
+    </div>
+  );
 }
--- src/components/pages/GraphPage.js
+++ src/components/pages/GraphPage.js
@@ -1,77 +1,78 @@
-import React, { useState } from 'react';
-import Grid from '@material-ui/core/Grid';
-import { makeStyles } from '@material-ui/core/styles';
-import NavBar from '../navigation/NavBar';
-import Checkbox from '../objects/Checkbox'
-import { ProcessGraph } from '../objects/ProcessGraph';
+import React, { useState } from "react";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
+import NavBar from "../navigation/NavBar";
+import { CheckBoxGroup } from "../objects/Checkbox";
+import { ProcessGraph } from "../objects/ProcessGraph";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
 
 const useStyles = makeStyles(theme => ({
-    graph: {
-        height: "90%"
-    },
-    gridCon: {
-        height: "100%"
-    }
+  graph: {
+    height: "90%"
+  },
+  gridCon: {
+    height: "100%"
+  }
 }));
 
 export default function GraphPage() {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    const [graphData, setGraphData] = useState([
-        {
-            name: 'graph1',
-            data: [{ x: 1, y: 3 }, { x: 2, y: 5 }, { x: 3, y: 15 }, { x: 4, y: 12 }]
-        },
-        {
-            name: 'graph2',
-            data: [{ x: 1, y: 10 }, { x: 2, y: 4 }, { x: 3, y: 2 }, { x: 4, y: 15 }]
-        },
-        {
-            name: 'graph3',
-            data: [{ x: 1, y: 7 }, { x: 2, y: 11 }, { x: 3, y: 9 }, { x: 4, y: 2 }]
-        }
-    ]
-    )
-
-    //map function: you take something and for everything you do something
-    //filter function: check everything you need against it and filter out what you don't need 
-
-    //TODO: populate when we pull from the saved configs 
-    const [graphNameMap, setGraphNameMap] = useState(
-        {
-            mrFig: false,
-            name2: false,
-            name3: true,
-        },
-    );
-
-    const onCheck = (name) => {
-        //access graphNames and flip boolean 
-        // graphNames[name] = !graphNames[name];
-
-        setGraphNameMap((currentGraphNameMap) => {
-            currentGraphNameMap[name] = !currentGraphNameMap[name]
-
-            console.log(currentGraphNameMap);
-
-            return currentGraphNameMap
-        })
-
-        // alert(graphNameMap[name])
+  const [graphData, setGraphData] = useState([
+    {
+      name: "mrFig",
+      data: [{ x: 1, y: 3 }, { x: 2, y: 5 }, { x: 3, y: 15 }, { x: 4, y: 12 }]
+    },
+    {
+      name: "name2",
+      data: [{ x: 1, y: 10 }, { x: 2, y: 4 }, { x: 3, y: 2 }, { x: 4, y: 15 }]
+    },
+    {
+      name: "graph3",
+      data: [{ x: 1, y: 7 }, { x: 2, y: 11 }, { x: 3, y: 9 }, { x: 4, y: 2 }]
     }
+  ]);
 
-    return (
-        <>
-            <NavBar />
-            <Grid container className={classes.graph}>
-                <Grid item xs={12} sm={9}>
-                    <ProcessGraph graphData={graphData} />
-                </Grid>
-                <Grid item xs={12} sm={3}>
-                    <Checkbox onCheck={onCheck} graphNameMap={graphNameMap} />
-                    {/* pass the checkstate stuff in here */}
-                </Grid>
-            </Grid>
-        </>
-    );
+  //map function: you take something and for everything you do something
+  //filter function: check everything you need against it and filter out what you don't need
+
+  //TODO: populate when we pull from the saved configs
+  const [graphNameMap, setGraphNameMap] = useState({
+    mrFig: false,
+    name2: false,
+    graph3: true
+  });
+
+  const onCheck = name => event => {
+    setGraphNameMap({
+      ...graphNameMap,
+      [name]: event.target.checked
+    });
+  };
+
+  return (
+    <>
+      <NavBar />
+      <Grid container className={classes.graph}>
+        <Grid item xs={12} sm={9}>
+          <ProcessGraph graphData={graphData.filter(g => graphNameMap[g.name])} />
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <CheckBoxGroup>
+            {Object.entries(graphNameMap).map(([name, toggleOn]) => (
+              <FormControlLabel
+                control={
+                  <Checkbox checked={toggleOn} onChange={onCheck(name)} />
+                }
+                label={name}
+              />
+            ))}
+          </CheckBoxGroup>
+          {/* <Checkbox onCheck={onCheck} graphNameMap={graphNameMap} /> */}
+          {/* pass the checkstate stuff in here */}
+        </Grid>
+      </Grid>
+    </>
+  );
 }
