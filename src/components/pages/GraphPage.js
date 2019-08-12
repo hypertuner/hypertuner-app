210bfd4dfacbeff5b9281233ec60abb29a4b549a
Dian Niu <zn15@duke.edu>
2019-08-12 00:18:23 -0400

    trying to merge lmao

--- src/components/pages/GraphPage.js
+++ src/components/pages/GraphPage.js
@@ -31,10 +31,7 @@ export default function GraphPage() {
             name: 'graph3',
             data: [{ x: 1, y: 7 }, { x: 2, y: 11 }, { x: 3, y: 9 }, { x: 4, y: 2 }]
         }
-    ]
-        // [{ x: 1, y: 3 }, { x: 2, y: 5 }, { x: 3, y: 15 }, { x: 4, y: 12 }],
-        // [{ x: 1, y: 10 }, { x: 2, y: 4 }, { x: 3, y: 2 }, { x: 4, y: 15 }],
-        // [{ x: 1, y: 7 }, { x: 2, y: 11 }, { x: 3, y: 9 }, { x: 4, y: 2 }]
+       ]
     )
 
     //TODO: populate when we pull from the saved configs 
