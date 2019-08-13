64d8aa9e420e2440790d74b8c4db7ab8cef46100
Klemen Kotar <klemen@cs.washington.edu>
2019-08-13 15:09:02 -0700

    Added functionality for recieving model progress data in the front end

--- src/components/objects/ConfigGrid.js
+++ src/components/objects/ConfigGrid.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import ConfigTab from "../buttons/ConfigTab";
+import { progressSocket, progressWatch, progressUnwatch } from "../../api/actionSocket";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -20,6 +21,36 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
+const [processStatuses, setProcessStatuses] = useState({});
+const [progressWatchId, setProgressWatchId] = useState();
+
+useEffect(() => {
+  const handleMessage = ({ data }) => {
+    const jsonData = JSON.parse(data);
+
+    if (!jsonData.success) return;
+    console.log(jsonData);
+
+    if (jsonData.type === "progress-stop") {
+      setProgressWatchId(null);
+    }
+
+    if (jsonData.type === "progress-data") {
+      setProcessStatuses(jsonData.processStatus);
+      setProgressWatchId(jsonData.watchId);
+      console.log(jsonData.processStatus);
+    }
+  };
+
+  progressWatch();
+  progressSocket.addEventListener("message", handleMessage)
+
+  return () => {
+    progressSocket.removeEventListener("message", handleMessage);
+    progressUnwatch(progressWatchId);
+  };
+// eslint-disable-next-line react-hooks/exhaustive-deps
+}, []);
 
 export default function FullWidthGrid({transition, configList, setConfigList}) {
   const classes = useStyles();
