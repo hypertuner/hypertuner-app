37f43f2fbae0615b12c51c788a1c7c7c3405c794
Aman Arya <aman.arya524@gmail.com>
2019-08-14 17:23:22 -0700

    Howdy ho

--- src/components/navigation/NavBar.js
+++ src/components/navigation/NavBar.js
@@ -1,34 +1,32 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import {Link} from 'react-router-dom';
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import { Link } from 'react-router-dom'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
 
 const useStyles = makeStyles(theme => ({
   root: {
-    height: "10%"
+    height: '10%'
   },
   button: {
     margin: theme.spacing(2),
-    textDecoration: "inherit",
-    color: "inherit"
+    textDecoration: 'inherit',
+    color: 'inherit'
   },
   menuButton: {
     marginRight: theme.spacing(2)
   },
   toolbarButtons: {
     // margin: theme.spacing(1.5),
-    marginLeft: "auto",
+    marginLeft: 'auto',
     marginRight: -15
   }
-}));
+}))
 
 export default function NavBar() {
-  const classes = useStyles();
+  const classes = useStyles()
 
   return (
     <div className={classes.root}>
@@ -42,28 +40,30 @@ export default function NavBar() {
           >
             <MenuIcon />
           </IconButton> */}
-          <Typography style={{fontFamily: "Minimalust"}} variant="h5" color="inherit">
-            hypErtUner
-          </Typography>
+          <a
+            href="https://hypertuner.github.io"
+            target="_blank"
+            rel="noopener noreferrer" 
+            style={{ color: 'white' }}>
+            <Typography
+              style={{ fontFamily: 'Minimalust', color: 'white' }}
+              variant="h5">
+              hypErtUner
+            </Typography>
+          </a>
           <span className={classes.toolbarButtons}>
-            <Link className={classes.button} to='/' >
-              <Button className={classes.button}>
-                Configs
-              </Button>
+            <Link className={classes.button} to="/">
+              <Button className={classes.button}>Configs</Button>
             </Link>
-            <Link className={classes.button} to='/graph'>
-              <Button className={classes.button}>
-                Graphs
-              </Button>
+            <Link className={classes.button} to="/graph">
+              <Button className={classes.button}>Graphs</Button>
             </Link>
-            <Link className={classes.button} to='/terminal'>
-              <Button className={classes.button}>
-                Terminal
-              </Button>
+            <Link className={classes.button} to="/terminal">
+              <Button className={classes.button}>Terminal</Button>
             </Link>
           </span>
         </Toolbar>
       </AppBar>
     </div>
-  );
+  )
 }
